ffa6b38ed7d03ddc8c48881217a1010d9ca51623
Chelsey S <chelseyann2119@gmail.com>
2021-01-15 19:01:21 -0700

    fixed crazy rounding error

--- src/makechange/MakeChange.java
+++ src/makechange/MakeChange.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class MakeChange {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		double itemPrice = getItemPrice(sc);
 		double payment = getPayment(sc);
 		makeChange(itemPrice, payment);
-		
+
 		sc.close();
 	}
 
@@ -17,54 +17,54 @@ public class MakeChange {
 		// First step is to check if they have given enough money
 		if (itemPrice > payment) {
 			System.out.println("Error: You did not give enough money to pay for item.");
-		}
-		
-		// If they have given enough money then subtract itemPrice from payment to get change
-		
-		double change = payment - itemPrice;
-		int bills = (int) change;
-		int coins = (int) (change * 100 - bills * 100);
-		
-		// convert change into denominations
-		System.out.print("Your change is: ");
-		if (bills >= 20) {
-			int denomination = (int) bills/20;
-			System.out.print(denomination + "-$20  ");
-			bills %= 20;
-		}
-		if (bills >= 10) {
-			int denomination = (int) bills/10;
-			System.out.print(denomination + "-$10  ");
-			bills %= 10;
-		}
-		if (bills >= 5) {
-			int denomination = (int) bills/5;
-			System.out.print(denomination + "-$5  ");
-			bills %= 5;
-		}
-		if (bills >= 1) {
-			System.out.print(bills + "-$1  ");
-		}
-		if (coins >= 25) {
-			int denomination = (int) coins/25;
-			System.out.print(denomination + "-25c  ");
-			coins %= 25;
-		}
-		if (coins >= 10 ) {
-			int denomination = (int) coins/10;
-			System.out.print(denomination + "-10c  ");
-			coins %= 10;
-		}
-		if (coins >= 5) {
-			int denomination = (int) coins/5;
-			System.out.print(denomination + "-5c  ");
-			coins %= 5;
-		}
-		if (coins >= 1) {
-			System.out.print(coins + "-1c  ");
+		} else {
+			// If they have given enough money then subtract itemPrice from payment to get
+			// change
+
+			double change = payment - itemPrice;
+			int bills = (int) change;
+			int coins = (int) Math.round((change- bills) * 100);
+
+			// convert change into denominations
+			System.out.print("Your change is: ");
+			if (bills >= 20) {
+				int denomination = (int) bills / 20;
+				System.out.print(denomination + "-$20  ");
+				bills %= 20;
+			}
+			if (bills >= 10) {
+				int denomination = (int) bills / 10;
+				System.out.print(denomination + "-$10  ");
+				bills %= 10;
+			}
+			if (bills >= 5) {
+				int denomination = (int) bills / 5;
+				System.out.print(denomination + "-$5  ");
+				bills %= 5;
+			}
+			if (bills >= 1) {
+				System.out.print(bills + "-$1  ");
+			}
+			if (coins >= 25) {
+				int denomination = (int) coins / 25;
+				System.out.print(denomination + "-25c  ");
+				coins %= 25;
+			}
+			if (coins >= 10) {
+				int denomination = (int) coins / 10;
+				System.out.print(denomination + "-10c  ");
+				coins %= 10;
+			}
+			if (coins >= 5) {
+				int denomination = (int) coins / 5;
+				System.out.print(denomination + "-5c  ");
+				coins %= 5;
+			}
+			if (coins >= 1) {
+				System.out.print(coins + "-1c  ");
+			}
 		}
 
-		
 	}
 
 	private static double getPayment(Scanner sc) {
@@ -73,19 +73,17 @@ public class MakeChange {
 		System.out.print("How much money are you giving clerk to pay for item? ");
 		double payment = sc.nextDouble();
 		sc.nextLine();
-		
+
 		return payment;
 	}
 
 	private static double getItemPrice(Scanner sc) {
 		// This asks user for the price of their item and returns it as a double
-		
+
 		System.out.print("What is the price of the item you're buying today? ");
 		double itemPrice = sc.nextDouble();
-		
+
 		return itemPrice;
 	}
-	
-	
-	
+
 }
